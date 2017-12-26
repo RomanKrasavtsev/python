79bda4e82265361a025bc30c9376b7626b61a771
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2017-12-26 17:19:53 +0100

    add personal info

---
+++ personal_info.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+first_name = "Roman"
+last_name = "Krasavtsev"
+age = "31"
+birth_date = "01.01.1986"
+
+print("My name is %s %s.\nI was born on %s and I'm %s years old." % (first_name, last_name, birth_date, age))
