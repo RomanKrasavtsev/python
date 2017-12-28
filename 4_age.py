a924ba702b23b814293e2975348d2a2e117b8ccb
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2017-12-28 11:12:13 +0100

    add age

---
+++ 4_age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+name = raw_input("What is your name? ")
+birthdate = raw_input("What is your bithdate? ")
+age = input("How old are you? ")
+
+print("%s was born on %s" % (name, birthdate))
+print("Half of your age is %s" % (age / 2))
